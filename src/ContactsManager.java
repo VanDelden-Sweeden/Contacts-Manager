eba7d32a274c7636421eded56f12994cfdaa14b7
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-01-18 15:14:37 -0700

    saving just in case

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -6,34 +6,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class ContactsManager {
     public Scanner scanner;
     public static String userIn;
+    private static int count;
+    private static List<String> newContactCollection;
 
-    public static void displayList(List<String> contactCollection) {
-        for (int i = 0; i < contactCollection.size(); i += 1) {
-            System.out.println((i + 1) + ": " + contactCollection.get(i));
-        }
-        displayMenu(contactCollection);
-    }
-
-    public static void addContact(List<String> contactCollection) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter name and phone number: ");
-        userIn = scanner.nextLine();
-        contactCollection.add(userIn);
-        displayMenu(contactCollection);
-    }
-
-    public static void displayMenu(List<String> contactCollection) {
+    public static void displayMenu(List<String> contactCollection) throws IOException {
         System.out.println("Please select a menu option: ");
         System.out.println("1 - Display Contacts");
         System.out.println("2 - Add Contact");
-        System.out.println("3 - ");
-        System.out.println("4 - ");
+        System.out.println("3 - Search for a Contact");
+        System.out.println("4 - Delete a Contact");
         System.out.println("5 - Exit");
-//        userIn = "1";
+        System.out.println("Enter the number of your selection: ");
         Scanner scanner = new Scanner(System.in);
         userIn = scanner.nextLine();
         if (userIn.equals("1")) {
@@ -41,14 +27,78 @@ public class ContactsManager {
         } else if (userIn.equals("2")) {
             addContact(contactCollection);
         } else if (userIn.equals("3")) {
-            displayMenu(contactCollection);
+            searchContact(contactCollection);
         } else if (userIn.equals("4")) {
             displayMenu(contactCollection);
         } else if (userIn.equals("5")) {
-            displayMenu(contactCollection);
+            System.out.println("Thank you for using Contacts Manager!");
+            Path filepath = Paths.get("data", "contacts.txt");
+            Files.write(filepath, contactCollection);
         } else {
+            System.out.println("Please select a valid entry.");
             displayMenu(contactCollection);
         }
+    }
+
+    public static void displayList(List<String> contactCollection) throws IOException {
+        for (int i = 0; i < contactCollection.size(); i += 1) {
+            System.out.println((i + 1) + ": " + contactCollection.get(i));
+        }
+        displayMenu(contactCollection);
+    }
+
+    public static void addContact(List<String> contactCollection) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name and phone number: ");
+        userIn = scanner.nextLine();
+        contactCollection.add(userIn);
+        displayMenu(contactCollection);
+    }
+
+    public static void searchContact(List<String> contactCollection) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Search for: ");
+        userIn = scanner.nextLine();
+        for (String contact : contactCollection) {
+            if (contact.contains(userIn)) {
+                System.out.println(count + ": contact = " + contact);
+                count++;
+            }
+        }
+        displayMenu(contactCollection);
+    }
+
+    public static void deleteContact(List<String> contactCollection) throws IOException {
+        for (int i = 0; i < contactCollection.size(); i += 1) {
+            System.out.println((i + 1) + ": " + contactCollection.get(i));
+        }
+        System.out.println("Type the name of your contact and/or a full phone number: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Contact to delete: ");
+        userIn = scanner.nextLine();
+        count = 0;
+        for (String contact : contactCollection) {
+            if (contact.contains(userIn)) {
+                System.out.println("{y/n} Do you wish to delete " + userIn + "?");
+                count++;
+                userIn = scanner.nextLine();
+                if (userIn.equals("y")) {
+                    for (String contactD : contactCollection) {
+                        if (!contactD.contains(userIn)) {
+                        newContactCollection.add(contactD);
+                        }
+                    }
+                } else {
+                    deleteContact(contactCollection);
+                }
+            }
+
+        }
+        if (count == 0) {
+            System.out.println(userIn + " was not found.");
+            deleteContact(contactCollection);
+        }
+        displayMenu(contactCollection);
     }
 
     public static void main(String[] args) throws IOException {
